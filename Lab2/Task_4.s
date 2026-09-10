3cce47cb944c91e8727d4922f00fcb9cbc671851
Sal725626 <salman.mohammededu@gmail.com>
2026-09-10 17:06:43 +0000

    task4compile_Good

--- Lab2/Task_4.s
+++ Lab2/Task_4.s
@@ -8,19 +8,19 @@
 fill_ram:
     # calc the series and store final sum into given location
 
-  mov0 $0, %eax     # eax register will hold the sum; starts at = 0
-  mov1 $1, %ecx     #ecx will be the counter; starts at = 1
+movl $0, %eax     # eax register will hold the sum; starts at = 0
+movl $1, %ecx     #ecx will be the counter; starts at = 1
 
 loop:
-add1 %ecx, %eax     # sum= sum + counter
-add1 %1,    %ecx    # counter = counter + 1
+addl %ecx, %eax     # sum= sum + counter
+addl $1,    %ecx    # counter = counter + 1
 
 cmpl $10, %ecx      # compare counter to value of 10
 
 jle loop            # Repeat until counter <= 10
 
 
-movb $al, ram+0x50  #store our sum into given location which is 0x50
+movb %al, ram+0x50  #store our sum into given location which is 0x50
 
  ret     # Return ontrol back to C program
 
